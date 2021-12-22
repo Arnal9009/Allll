c0dc2a0458b2b338a5a37fde06cda3dcd6cb5eaf
Arnal9009 <arnalkadyrov61@gmail.com>
2021-12-22 18:39:39 +0600

    first commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2efbd802494558b8"; }
+/******/ 		__webpack_require__.h = function() { return "4d34a5403a228654"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.740bed7300f3f241.hot-update.js
+++ .next/static/webpack/webpack.740bed7300f3f241.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2651bc9b6aa62096"; }
+/******/ 	__webpack_require__.h = function() { return "5a44e738b8022978"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
