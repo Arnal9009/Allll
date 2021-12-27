33aad777b92e835696fff45ea750499131c6598c
Arnal <78338125+Arnal9009@users.noreply.github.com>
2021-12-27 14:55:51 +0600

    Add files via upload

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+import styles from "./Navbar.module.scss"
+
+import { BiMenuAltRight } from 'react-icons/bi'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+
+const Navbar = () => {
+  const [sideBar, seSideBar] = useState(false)
+  return (
+    <div className={styles.burger}>
+      <BiMenuAltRight fill='white' size={50} onClick={() => seSideBar(!sideBar)}>изменить</BiMenuAltRight>
+      {
+        sideBar ? <div className={styles.sideBar}>
+          <HiArrowNarrowRight fill='white' size={50} onClick={() => seSideBar(!sideBar)}/>
+          <ul>
+            <li>Places</li>
+            <li>About us</li>
+            <li>Log in</li>
+            <li>Search</li>
+          </ul>
+        </div> : null
+      }
+    </div>
+  )
+}
+
+export default Navbar
